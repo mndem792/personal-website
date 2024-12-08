3227cc0e592eabaa620f84ba31430077faaa2b9f
Merab Malishava <merabmalishava@gmail.com>
2024-12-08 22:51:02 +0900

    Add and style text for about me section. Add max-width to articles page

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -7,15 +7,12 @@ export default function AboutMe() {
             </div>
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
-                    <p className="section--title"> Space Holder</p>
                     <h1 className="skills--section--heading">
                         About Me
                     </h1>
-                    <p className="hero--section--description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas deleniti suscipit sit saepe accusamus nemo mollitia rerum eos dolorem, in dolorum molestiae eveniet deserunt dolores, voluptatum culpa cupiditate consequatur harum?
-                    </p>
-                    <p className="hero--section--description">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, quaerat cupiditate? Provident, doloremque. Sed, fuga.
+                    <p className="hero--section-content">
+                    I am an Embedded & Software Engineer with a background in Computational Statistical Physics and Dynamical Systems. Being a person who walked into E&SW Engineering without prior knowledge of nuances such as specific terminology, toolsets, practices, etc. collecting information was a meticulous step by step process akin to constructing a puzzle. On this website I want to share the bits of information I learnt via short articles with the aim to help anybody who is in a position where I was originally. Here you will find information on lingo and practices in embedded engineering, insights into history and operation of Linux, creating your own Linux distros, bootloaders, drivers and many more.
+                        I am also a LeetCoding enthusiast with a proven expertise in performing on the interviews. I want to share my insights into the process, preparation strategies and thoughts on specific algorithms.                      
                     </p>
                 </div>
             </div>
