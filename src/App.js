471c3be45532e39d70cc30088dc9b66184323c73
Merab <merabmalishava.w@gmail.com>
2025-01-11 18:45:02 +0100

    Add a Wrapper to scroll to top if nav location changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate, useLocation} from 'react-router-dom';
 import Navbar from './Pages/Home/Navbar';
 import Home from "./Pages/Home/Homescreen"
@@ -9,10 +9,23 @@ import Article from './Pages/Home/ArticlesPage/Article';
 import NotFound from './Pages/Home/NotFound';
 
 
+const Wrapper = ({ children }) => {
+  const {pathname} = useLocation();
+
+  useLayoutEffect(() => {
+    // Scroll to the top of the page when the route changes
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [pathname]);
+
+  return null;
+};
+
+
 function App() {
   return (
     <div className="App">
       <Router>
+        <Wrapper/>
           <Navbar />
           <Routes>
             <Route path="/aboutme2" element={<Home />}/>
